1d32ffe7898018bd811ff2b683a60e5b9e873996
Diogo Matos <diogo.s.matos@tecnico.ulisboa.pt>
2022-05-14 00:47:49 +0100

    axios config

---
+++ src/api/httpClient.api.js
@@ -0,0 +1,77 @@
+import axios from 'axios';
+
+const httpClient = axios.create({
+	baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000',
+	headers: {
+		'Content-Type': 'application/json',
+	},
+});
+
+export const getAuthToken = () => localStorage.getItem('token');
+
+const authInterceptor = (config) => {
+	const token = getAuthToken();
+
+	if (token) config.headers['Authorization'] = `Bearer ${getAuthToken()}`;
+
+	return config;
+};
+
+// interceptor to catch errors
+const errorInterceptor = (error) => {
+	// check if it's a server error
+	if (!error.response) {
+		// if it's a server error, show a message
+		return Promise.reject(error);
+	}
+
+	if (error.config.skipInterceptor) {
+		return Promise.reject(error);
+	}
+
+	// all the other error responses
+	switch (error.response.status) {
+		case 400:
+			console.error(error.response.status, error.message);
+
+			break;
+
+		case 401: // authentication error, logout the user
+			// Will need update when login is finalized
+
+			localStorage.removeItem('token');
+			window.location.replace(window.location.origin);
+			break;
+
+		case 409:
+			break;
+
+		case 500:
+			console.error(error.response.status, error.message);
+			break;
+
+		default:
+			console.error(error.response.status, error.message);
+	}
+	return Promise.reject(error);
+};
+
+// Interceptor for responses
+// Useless for now but gotta be there i guess for axios' response interceptor, might update in the future for other success successful statuses
+const responseInterceptor = (response) => {
+	switch (response.status) {
+		case 200:
+			// yay!
+			break;
+		// any other cases
+		default:
+		// default case
+	}
+
+	return response;
+};
+
+httpClient.interceptors.request.use(authInterceptor);
+httpClient.interceptors.response.use(responseInterceptor, errorInterceptor);
+
+export default httpClient;
